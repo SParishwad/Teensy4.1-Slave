07e0fd6173ea652252bcb3146f01ca8115f133d3
SParishwad <36662803+SParishwad@users.noreply.github.com>
2021-06-22 20:42:14 +0200

    IMU Radio Communication

--- src/main.cpp
+++ src/main.cpp
@@ -18,19 +18,29 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-// int led = 13;
-/*******************************************************************/
 
-#define CE 10
-#define CSN 9
-//const byte addresses[][6] = {"00001", "00002"};
+#define CE_PIN 10
+#define CSN_PIN 9
+
 const byte address[6] = "00001";
-RF24 radio(CE, CSN);
-char ctrlData[32] = {0};    // this must match dataToSend in the TX
-int imuData[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // This array will be sent to the Master with the IMU Data. I can send upto 32 bytes of data at a time.
+RF24 radio(CE_PIN, CSN_PIN);
+
+char ctrlData[32] = {0};       // this must match dataToSend in the TX
+int imuData[2] = {109, -4000}; // the two values to be sent to the master
 bool newData = false;
 /*******************************************************************/
 
+/*******************************************************************/
+/**
+ * Waveshare 10 DOF IMU (Lib: Bolderflight I2C)
+ */
+#include <MPU9250.h>
+MPU9250 IMU_Left(Wire, 0x68);   // "Wire" object refers to pins 18/19
+MPU9250 IMU_Right(Wire1, 0x68); // "Wire1" Object refers to pins 16/17 and needs to be initialized in Setup
+int status;
+/*******************************************************************/
+
+/*******************************************************************/
 /**
  * Servo Motor Control
  */
@@ -49,39 +59,21 @@ int offsetRightAileron = 100;
 int offsetBLDC = 90;
 /*******************************************************************/
 
-/**
- * Waveshare 10 DOF IMU (Lib: Bolderflight I2C)
- */
-#include <MPU9250.h>
-MPU9250 IMU_Left(Wire, 0x68);   // "Wire" object refers to pins 18/19
-MPU9250 IMU_Right(Wire1, 0x68); // "Wire1" Object refers to pins 16/17 and needs to be initialized in Setup
-int status;
-/*******************************************************************/
-
 void setup()
 {
+  /*******************************************************************/
   Serial.begin(115200);
-  delay(1000);
-  // pinMode(led, OUTPUT);
 
-  //Servo Initialization (NodeMCU)
-  Serial.println("Initializing Servos...");
-  servoElevator.attach(5);  //5
-  servoElevator.write(offsetElevator);
-  delay(200);
-  servoRudder.attach(6);  //4
-  servoRudder.write(offsetRudder);
-  delay(200);
-  servoBLDC.attach(2); //14
-  servoLeftAileron.attach(3);  //0
-  servoLeftAileron.write(offsetLeftAileron);
-  delay(200);
-  servoRightAileron.attach(4);  //2
-  servoRightAileron.write(offsetRightAileron);
-  delay(200);
-  servoBLDC.write(0);
-  Serial.println("Servos Initialized!");
+  // Radio Communication (nRF24L01+LA+PNA)
+  Serial.println("SimpleRxAckPayload Starting");
+  radio.begin();
+  radio.setDataRate(RF24_250KBPS);
+  radio.enableAckPayload();
+  radio.openReadingPipe(1, address);
+  radio.startListening();
+  radio.writeAckPayload(1, &imuData, sizeof(imuData)); // pre-load data
 
+  /*******************************************************************/
   // 10 DOF IMU Initialization
   Serial.println("Initializing IMUs...");
   Wire1.setSCL(16);
@@ -117,30 +109,48 @@ void setup()
   IMU_Right.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ); // setting DLPF bandwidth to 20 Hz
   IMU_Right.setSrd(19);                                     // setting SRD to 19 for a 50 Hz update rate
   Serial.println("IMUs Initialized!");
-
-  //Radio Communication (nRF24L01+LA+PNA)
-  Serial.println("Radio Communnication Starting...");
-  //Serial.println(addresses[0]);
-  radio.begin();
-  radio.setDataRate( RF24_250KBPS );
-  radio.openReadingPipe(0, address); // 00001
-  radio.enableAckPayload();
-  radio.startListening();
-  radio.writeAckPayload(0, &imuData, sizeof(imuData)); // pre-load data
-  /* radio.openWritingPipe(addresses[1]);    // 00002
-  radio.openReadingPipe(0, addresses[0]); // 00001
-  radio.setPALevel(RF24_PA_MIN); */
-  delay(1000);
-  Serial.println("Radio Communication Started!");
-  //radio.startListening();
+  
+  /*******************************************************************/
+  //Servo Initialization (NodeMCU)
+  Serial.println("Initializing Servos...");
+  servoElevator.attach(5);
+  servoElevator.write(offsetElevator);
+  delay(200);
+  servoRudder.attach(6);
+  servoRudder.write(offsetRudder);
+  delay(200);
+  servoBLDC.attach(2);
+  servoLeftAileron.attach(3);
+  servoLeftAileron.write(offsetLeftAileron);
+  delay(200);
+  servoRightAileron.attach(4);
+  servoRightAileron.write(offsetRightAileron);
+  delay(200);
+  servoBLDC.write(0);
+  Serial.println("Servos Initialized!");
+  /*******************************************************************/
 }
 
-void updateReplyData() {
+/*void updateReplyData() {
+    ackData[0] -= 1;
+    ackData[1] -= 1;
+    if (ackData[0] < 100) {
+        ackData[0] = 109;
+    }
+    if (ackData[1] < -4009) {
+        ackData[1] = -4000;
+    }
+    radio.writeAckPayload(1, &ackData, sizeof(ackData)); // load the payload for the next time
+}*/
+
+/*******************************************************************/
+void updateReplyData()
+{
   IMU_Left.readSensor();
   IMU_Right.readSensor();
   imuData[0] = IMU_Left.getAccelX_mss(); // Convert the variable to char
   imuData[1] = IMU_Right.getAccelX_mss();
-  imuData[2] = IMU_Left.getAccelY_mss();
+  /*imuData[2] = IMU_Left.getAccelY_mss();
   imuData[3] = IMU_Right.getAccelY_mss();
   imuData[4] = IMU_Left.getAccelZ_mss();
   imuData[5] = IMU_Right.getAccelZ_mss();
@@ -160,55 +170,21 @@ void updateReplyData() {
   imuData[17] = IMU_Right.getMagZ_uT();
 
   imuData[18] = IMU_Left.getTemperature_C();
-  imuData[19] = IMU_Right.getTemperature_C();
-  radio.writeAckPayload(0, &imuData, sizeof(imuData)); // load the payload for the next time
+  imuData[19] = IMU_Right.getTemperature_C();*/
+  radio.writeAckPayload(1, &imuData, sizeof(imuData)); // load the payload for the next time
 }
+/*******************************************************************/
 
-
-
-void loop()
+void servoActuation()
 {
-  //radio.read(&ctrlData, sizeof(ctrlData));
-  //Serial.println("Debug");
-  //Serial.print(ctrlData);
-  
-  if (radio.available())
-  {
-    radio.read(&ctrlData, sizeof(ctrlData));
-    Serial.println(ctrlData);
-    joystickID = strtok((char *)ctrlData, ";");
-    if (*joystickID == 'R')
-    {
-      cRightX = strtok(NULL, ";");
-      rightX = atoi(cRightX);
-      cRightY = strtok(NULL, ";");
-      rightY = atoi(cRightY);
-      Serial.print("Right Joystick:   ");
-      Serial.print(atof(cRightX));
-      Serial.print("\t");
-      Serial.print(atof(cRightY));
-      Serial.print("\n");
-    }
-    else if (*joystickID == 'L')
-    {
-      cLeftX = strtok(NULL, ";");
-      leftX = atoi(cLeftX);
-      cLeftY = strtok(NULL, ";");
-      leftY = atoi(cLeftY);
-      Serial.print("Left Joystick:   ");
-      Serial.print(atof(cLeftX));
-      Serial.print("\t");
-      Serial.print(atof(cLeftY));
-      Serial.print("\n");
-    }
-    newData = true;
-  }
-  //radio.stopListening();
-
   if (newData == true)
   {
-    Serial.print("LeftAileron: ");
-    Serial.println(rightX + offsetLeftAileron);
+    Serial.print("Data received ");
+    Serial.println(ctrlData);
+    Serial.print(" ackPayload sent ");
+    Serial.print(imuData[0]);
+    Serial.print(", ");
+    Serial.println(imuData[1]);
     servoElevator.write(rightY + offsetElevator);
     servoRudder.write(leftX + offsetRudder);
     servoLeftAileron.write(rightX + offsetLeftAileron);
@@ -216,6 +192,65 @@ void loop()
     servoBLDC.write(offsetBLDC - leftY);
     newData = false;
   }
+}
+/*******************************************************************/
 
-  updateReplyData();
+void getData()
+{
+  if (radio.available())
+  {
+    radio.read(&ctrlData, sizeof(ctrlData));
+    joystickID = strtok((char *)ctrlData, ";");
+    if (*joystickID == 'R')
+    {
+      cRightX = strtok(NULL, ";");
+      rightX = atoi(cRightX);
+      cRightY = strtok(NULL, ";");
+      rightY = atoi(cRightY);
+      /*Serial.print("Right Joystick:   ");
+          Serial.print(atof(cRightX));
+          Serial.print("\t");
+          Serial.print(atof(cRightY));
+          Serial.print("\n");*/
+    }
+    else if (*joystickID == 'L')
+    {
+      cLeftX = strtok(NULL, ";");
+      leftX = atoi(cLeftX);
+      cLeftY = strtok(NULL, ";");
+      leftY = atoi(cLeftY);
+      /*Serial.print("Left Joystick:   ");
+          Serial.print(atof(cLeftX));
+          Serial.print("\t");
+          Serial.print(atof(cLeftY));
+          Serial.print("\n");*/
+    }
+    updateReplyData();
+    newData = true;
+  }
+}
+
+//================
+
+void showData()
+{
+  if (newData == true)
+  {
+    Serial.print("Data received ");
+    Serial.println(ctrlData);
+    Serial.print(" ackPayload sent ");
+    Serial.print(imuData[0]);
+    Serial.print(", ");
+    Serial.println(imuData[1]);
+    newData = false;
+  }
+}
+
+//================
+
+void loop()
+{
+  getData();
+  //showData();
+  servoActuation();
 }
